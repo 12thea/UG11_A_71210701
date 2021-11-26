16f58606a531e41f0b153a25fe377790a8159dea
thea natasia <93317808+12thea@users.noreply.github.com>
2021-11-26 09:56:46 +0700

    Update 3_A_71210701.py

--- 3_A_71210701.py
+++ 3_A_71210701.py
@@ -1,10 +1,13 @@
 def hitung_hapus():
-    k = input("Masukan Kalimat: ")
-    is1 = int(input("Index Start: "))
-    is2 = int(input("Index Stop: "))
-    h = ((is1-is2+1)/len(k))*100
-    if h < 0:
-        return 0.0
-    else:
-        return h
+    k = input("Masukan Kalimat : ")
+    is1 = int(input("Index Start : "))
+    is2 = int(input("Index Stop : "))
+
+    tjd = str(len(k))
+    A = k[is1-1:is2]
+    hapus = str(len(A))
+    hasil = (int(hapus)/int(tjd))*100
+    return hasil
+
+
 print(hitung_hapus())
